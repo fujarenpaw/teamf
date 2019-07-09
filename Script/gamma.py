a20bbfbe404ae427bcde4be9a83849afd67d5dd7
fujarenpaw <nairo@nuinui.fml.org>
2019-07-10 00:58:18 +0900

    ガンマ補正　コメント追加

--- Script/gamma.py
+++ Script/gamma.py
@@ -9,6 +9,8 @@ import sys
 """
 入力した画像にgamma補正をかけて出力する。下記のように実行
 python gammma.py hogehoge.jpg(画像パス) 230(level)
+300を倍率1(無補正として)0に近いと白く、正の値が大きいと黒くなる
+
 """
 if __name__ == '__main__':
     args = sys.argv
